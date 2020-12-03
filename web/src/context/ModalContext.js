26936ba188b2f2051e5edf5b5a530694452613c0
Ryan Doyle <doylecodes@gmail.com>
2020-12-03 14:34:57 -0800

    create modal context

---
+++ web/src/context/ModalContext.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const ModalContext = React.createContext(null)
+ModalContext.displayName = 'ModalContext'
+
+const ModalProvider = ({ children }) => {
+  const [childComponent, setChildComponent] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <ModalContext.Provider
+      value={{
+        child: childComponent,
+        setChild: (component) => {
+          setChildComponent(component)
+        },
+        isOpen: isOpen,
+        open: () => setIsOpen(true),
+        close: () => setIsOpen(false),
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  )
+}
+
+export { ModalProvider }
+export default ModalContext
