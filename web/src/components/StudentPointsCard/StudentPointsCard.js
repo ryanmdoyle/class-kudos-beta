0494b32bcf1ff81a369446c3aae970ff023d2c0e
Ryan <ryandoyle@me.com>
2021-01-04 10:31:08 -0800

    make smaller

--- web/src/components/StudentPointsCard/StudentPointsCard.js
+++ web/src/components/StudentPointsCard/StudentPointsCard.js
@@ -4,7 +4,7 @@ const StudentPointsCard = ({ student, points }) => {
   const { id, firstName, lastName } = student || {}
 
   return (
-    <div className="h-48 white-box mt-1 mb-4 flex flex-col relative">
+    <div className="h-36 white-box mt-1 mb-4 flex flex-col relative">
       <h1 className="text-3xl font-display mb-4">
         {firstName} {lastName}
       </h1>
