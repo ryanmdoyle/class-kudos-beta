4ddee2bc9386107a395c90b070bbcef1740fe9c6
Ryan Doyle <doylecodes@gmail.com>
2021-01-20 15:13:35 -0800

    resolve conflicts

--- web/src/components/StudentPointsCard/StudentPointsCard.js
+++ web/src/components/StudentPointsCard/StudentPointsCard.js
@@ -1,13 +1,7 @@
 import FeedbackPointsCell from 'src/components/cells/FeedbackPointsCell/FeedbackPointsCell'
 
-<<<<<<< HEAD
 const StudentPointsCard = ({ student }) => {
   const { id, firstName, lastName } = student || {}
-=======
-const StudentPointsCard = ({ student, points }) => {
-  const { id, firstName, lastName } = student || {}
-
->>>>>>> 0494b32bcf1ff81a369446c3aae970ff023d2c0e
   return (
     <div className="h-36 white-box mt-1 mb-4 flex flex-col relative">
       <h1 className="text-3xl font-display mb-4">
@@ -15,11 +9,7 @@ const StudentPointsCard = ({ student, points }) => {
       </h1>
       <div className="flex justify-end absolute bottom-2 right-4">
         <span className="font-display text-9xl text-green mr-8">
-<<<<<<< HEAD
           {id && <FeedbackPointsCell userId={id} />}
-=======
-          <FeedbackPointsCell userId={id} loadingValue={points} />
->>>>>>> 0494b32bcf1ff81a369446c3aae970ff023d2c0e
         </span>
       </div>
     </div>
