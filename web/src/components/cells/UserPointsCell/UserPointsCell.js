2d66fc12f9c9ae463e06a7fddd09674ac8319963
Ryan Doyle <doylecodes@gmail.com>
2021-02-25 17:41:44 -0800

    spinner

--- web/src/components/cells/UserPointsCell/UserPointsCell.js
+++ web/src/components/cells/UserPointsCell/UserPointsCell.js
@@ -16,7 +16,23 @@ export const beforeQuery = (props) => {
   }
 }
 
-export const Loading = () => '...'
+export const Loading = () => {
+  return (
+    <div className="w-6 h-6 text-purple-100 animate-spin">
+      <svg
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="transparent"
+        stroke="currentColor"
+        strokeWidth="12"
+        strokeDasharray="175"
+        strokeLinecap="round"
+      >
+        <circle cx="50" cy="50" r="35" />
+      </svg>
+    </div>
+  )
+}
 
 export const Empty = () => 0
 
