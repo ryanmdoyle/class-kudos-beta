3c659ae67a7c4a29a3bfc82710754dfd39795494
Ryan Doyle <doylecodes@gmail.com>
2021-03-23 14:00:59 -0700

    create wrapper for student points

--- web/src/pages/StudentGroupPage/StudentGroupPage.js
+++ web/src/pages/StudentGroupPage/StudentGroupPage.js
@@ -1,47 +1,12 @@
 import { useAuth } from '@redwoodjs/auth'
 import DashboardLayout from 'src/layouts/DashboardLayout/DashboardLayout'
-import UserPointsOfGroupCell from 'src/components/cells/UserPointsOfGroupCell/UserPointsOfGroupCell'
-import RewardsOfGroupStudentCell from 'src/components/cells/RewardsOfGroupStudentCell/RewardsOfGroupStudentCell'
-import UserFeedbackOfGroupCell from 'src/components/cells/UserFeedbackOfGroupCell/UserFeedbackOfGroupCell'
-import UserPointsCell from 'src/components/cells/UserPointsCell/UserPointsCell'
+import UserPointsWrapperCell from 'src/components/cells/UserPointsWrapperCell/UserPointsWrapperCell'
 
 const StudentGroupPage = ({ groupId }) => {
   const { currentUser } = useAuth()
-  const totalPoints = <UserPointsCell userId={currentUser?.id} />
   return (
     <DashboardLayout>
-      <div className="w-full h-full p-4 relative">
-        <div className="white-box w-full h-32 mb-4 relative">
-          <span className="text-8xl text-green-400">
-            <UserPointsCell userId={currentUser?.id} />
-          </span>
-          <span className="text-lg ml-2 text-gray-500 mr-12">
-            total {`${totalPoints === 1 ? 'point' : 'points' || 'points'}`}
-          </span>
-          {/* <span className="text-6xl text-green-400">
-            <UserPointsOfGroupCell groupId={groupId} userId={currentUser?.id} />
-          </span>
-          <span className="text-lg ml-2 text-gray-500">group points</span> */}
-        </div>
-        <div className="h-5/6 w-full flex relative">
-          <div className="white-box w-1/2 h-full p-2 mr-4">
-            <h2 className="font-display text-2xl mb-2">
-              Recent Feedback For Group
-            </h2>
-            <UserFeedbackOfGroupCell
-              groupId={groupId}
-              userId={currentUser?.id}
-            />
-          </div>
-          <div className="white-box w-1/2 h-full p-2">
-            <h2 className="font-display text-2xl mb-2">Rewards Available</h2>
-            <RewardsOfGroupStudentCell
-              groupId={groupId}
-              userId={currentUser?.id}
-            />
-          </div>
-        </div>
-      </div>
+      <UserPointsWrapperCell userId={currentUser?.id} groupId={groupId} />
     </DashboardLayout>
   )
 }
