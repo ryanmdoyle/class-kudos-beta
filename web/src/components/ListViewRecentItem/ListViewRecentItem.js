9cc964fc8d939d5509dc09af9658c566c4777382
Ryan Doyle <doylecodes@gmail.com>
2021-03-29 10:11:47 -0700

    reformat ListviewRecentItem value

--- web/src/components/ListViewRecentItem/ListViewRecentItem.js
+++ web/src/components/ListViewRecentItem/ListViewRecentItem.js
@@ -1,13 +1,14 @@
 const ListViewRecentItem = ({ name, value, createdAt }) => {
   const date = new Date(createdAt).toLocaleDateString()
+  const valueStyle = value > 0 ? 'text-green-500' : 'text-red-500'
   return (
     <li className="h-12 w-full white-box flex items-center justify-between px-4 mb-2">
       <div className="flex items-center">
+        <span className={`${valueStyle} font-bold w-10`}>{value}</span>
         <span className="text-normal">{name}</span>
       </div>
       <div className="justify-self-end">
-        {date && <span className="text-gray-500 font-light">{date} - </span>}
-        <span className="text-green-500 font-bold">{value}</span>
+        {date && <span className="text-gray-500 font-light">{date}</span>}
       </div>
     </li>
   )
