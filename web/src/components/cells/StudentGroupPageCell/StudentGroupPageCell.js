2def84a9ac7b136fe6ccd6b6ea2a59ed0a7e150f
Ryan <ryandoyle@me.com>
2021-04-10 12:18:36 -0700

    fixed headers on scrolling cards

--- web/src/components/cells/StudentGroupPageCell/StudentGroupPageCell.js
+++ web/src/components/cells/StudentGroupPageCell/StudentGroupPageCell.js
@@ -22,22 +22,23 @@ export const Success = ({ userId, groupId, group }) => {
   const points = <UserPointsCell userId={userId} />
   return (
     <div className="w-full h-full p-4 relative">
-      <div className="white-box w-full h-32 mb-4 relative">
+      <div className="white-box w-full h-1/6 mb-4 relative">
         <span className="text-8xl text-green-400">{points}</span>
         <span className="text-lg ml-2 text-gray-500 mr-12">
           total {`${points === 1 ? 'point' : 'points' || 'points'}`}
         </span>
-        {/* <span className="text-6xl text-green-400">
-            <UserPointsOfGroupCell groupId={groupId} userId={currentUser?.id} />
-          </span>
-          <span className="text-lg ml-2 text-gray-500">group points</span> */}
       </div>
       <div className="h-5/6 w-full flex relative">
-        <div className="white-box w-1/2 h-full p-2 mr-4">
-          <h2 className="font-display text-2xl mb-2">
-            Recent Feedback {group.name ? `for ${group.name}` : null}
-          </h2>
-          <UserActivityOfGroupCell groupId={groupId} userId={userId} />
+        <div className="bg-white rounded-md shadow w-1/2 h-full mr-4 overflow-scroll">
+          {/* use custom padding over 'white-box for list to scroll behind header */}
+          <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4">
+            <h2 className="font-display text-2xl">
+              Recent Feedback {group.name ? `for ${group.name}` : null}
+            </h2>
+          </div>
+          <div className="px-4">
+            <UserActivityOfGroupCell groupId={groupId} userId={userId} />
+          </div>
         </div>
         <div className="white-box w-1/2 h-full p-2">
           <h2 className="font-display text-2xl mb-2">Rewards Available</h2>
