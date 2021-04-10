65fa5d171abeb4f59a3661ceb88c1ebbc54a1370
Ryan <ryandoyle@me.com>
2021-04-10 12:18:00 -0700

    fixed headers on scrolling cards

--- web/src/components/RecentActivityListCard/RecentActivityListCard.js
+++ web/src/components/RecentActivityListCard/RecentActivityListCard.js
@@ -2,11 +2,15 @@ import UserActivityOfGroupCell from 'src/components/cells/UserActivityOfGroupCel
 
 const RecentActivityListCard = ({ userId, firstName, groupId, groupName }) => {
   return (
-    <div className="white-box mb-4 overflow-scroll">
-      <h2 className="font-display text-lg mb-2">
-        Recent Activity in {groupName} for {firstName}
-      </h2>
-      <UserActivityOfGroupCell userId={userId} groupId={groupId} />
+    <div className="bg-white rounded-md shadow mb-4 overflow-scroll">
+      <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4">
+        <h2 className="font-display text-lg">
+          Recent Activity in {groupName} for {firstName}
+        </h2>
+      </div>
+      <div className="px-4">
+        <UserActivityOfGroupCell userId={userId} groupId={groupId} />
+      </div>
     </div>
   )
 }
