6fd43771e89314dca48d15b92febf96b745b4c2f
Ryan Doyle <doylecodes@gmail.com>
2021-04-23 16:03:55 -0700

    seed data for db roles

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -9,14 +9,16 @@ async function main() {
   // Seed data is database data that needs to exist for your app to run.
   // Ideally this file should be idempotent: running it multiple times
   // will result in the same database state (usually by checking for the
-  // existence of a record before trying to create it). For example:
-  //
-  //   const existing = await db.user.findMany({ where: { email: 'admin@email.com' }})
-  //   if (!existing.length) {
-  //     await db.user.create({ data: { name: 'Admin', email: 'admin@email.com' }})
-  //   }
+  // existence of a record before trying to create it).
 
-  console.info('No data to seed. See api/prisma/seeds.js for info.')
+  const existing = await db.user.findUnique({
+    where: { email: 'doylecodes@gmail.com' },
+  })
+  if (existing !== null) {
+    await db.userRole.create({
+      data: { name: 'super_admin', userId: existing.id },
+    })
+  }
 }
 
 main()
