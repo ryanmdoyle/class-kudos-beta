a5dfac873d053955f8821f4ad594698bf2ae5178
Ryan Doyle <doylecodes@gmail.com>
2021-05-19 12:53:13 -0700

    center points in points card

--- web/src/components/cells/StudentGroupPageCell/StudentGroupPageCell.js
+++ web/src/components/cells/StudentGroupPageCell/StudentGroupPageCell.js
@@ -27,11 +27,13 @@ export const Success = ({ userId, groupId, group }) => {
   return (
     <div className="w-full h-full p-4 relative grid grid-cols-2 grid-rows-6 gap-2">
       {/* Points */}
-      <div className="white-box relative col-span-1 row-span-1 p-2">
+      <div className="white-box relative col-span-1 row-span-1 p-2 flex items-center">
+      <div className="pl-4">
         <span className="text-6xl text-green-400">{points}</span>
         <span className="text-lg ml-2 text-gray-500 mr-12">
           total {`${points === 1 ? 'point' : 'points' || 'points'}`}
         </span>
+      </div>
       </div>
       {/* Activity */}
       <div className="relative col-span-1 row-span-6">
@@ -39,7 +41,7 @@ export const Success = ({ userId, groupId, group }) => {
           {/* use custom padding over 'white-box for list to scroll behind header */}
           <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4">
             <h2 className="font-display text-2xl">
-              Recent Feedback {group?.name ? `for ${group.name}` : null}
+              Recent Feedback {group?.name ? `in ${group.name}` : null}
             </h2>
           </div>
           <div className="px-4">
@@ -49,7 +51,7 @@ export const Success = ({ userId, groupId, group }) => {
       </div>
       {/* Rewards */}
       <div className="white-box h-full p-2 col-span-1 row-span-5">
-        <h2 className="font-display text-2xl mb-2">Rewards Available</h2>
+        <h2 className="font-display text-2xl mb-2 p-2">Rewards Available</h2>
         <RewardsOfGroupStudentCell groupId={groupId} userId={userId} />
       </div>
     </div>
