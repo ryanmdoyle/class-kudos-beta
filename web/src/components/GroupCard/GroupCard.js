5d14dd287fe6e6ce0e53334c43e5383abdd2c151
Ryan Doyle <doylecodes@gmail.com>
2021-07-30 17:36:31 -0700

    adjust click handler area

--- web/src/components/GroupCard/GroupCard.js
+++ web/src/components/GroupCard/GroupCard.js
@@ -25,16 +25,15 @@ const GroupCard = ({
     <div
       className="w-100 h-24 white-box hover:shadow flex justify-between items-center mb-4 cursor-pointer"
       onClick={cardClick}
-      // data-click-element="card"
     >
       <div className="flex items-center">
         <div>
           <h2 className="text-xl font-display">{name}</h2>
           <span className="text-gray-500 text-sm">{description}</span>
           {enrollId && (
-            <span>
+            <span data-click-element="enrollId">
               <br></br>
-              <span className="text-gray-500 text-sm">Enroll ID: </span>
+              <span className="text-gray-500 text-sm" data-click-element="enrollId">Enroll ID: </span>
               <span
                 className="text-gray-500 text-sm font-mono"
                 data-click-element="enrollId"
@@ -67,13 +66,13 @@ const GroupCard = ({
             stroke="none"
             d="M0 0h24v24H0z"
             fill="none"
-            data-setting="true"
+            data-click-element="settings"
           />
           <path
             d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"
-            data-setting="true"
+            data-click-element="settings"
           />
-          <circle cx="12" cy="12" r="3" data-setting="true" />
+          <circle cx="12" cy="12" r="3" data-click-element="settings"/>
         </svg>
       </div>
     </div>
