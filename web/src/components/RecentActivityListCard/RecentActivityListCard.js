d1aee4314ba42b23acf036e0382d5ca61d1d2726
Ryan Doyle <doylecodes@gmail.com>
2021-09-05 16:38:50 -0700

    update props

--- web/src/components/RecentActivityListCard/RecentActivityListCard.js
+++ web/src/components/RecentActivityListCard/RecentActivityListCard.js
@@ -1,15 +1,15 @@
 import UserActivityOfGroupCell from 'src/components/cells/UserActivityOfGroupCell/UserActivityOfGroupCell'
 
-const RecentActivityListCard = ({ userId, firstName, groupId, groupName }) => {
+const RecentActivityListCard = ({ user, groupId, groupName }) => {
   return (
     <div className="bg-white rounded-md shadow mb-4 overflow-y-auto">
       <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4">
         <h2 className="font-display text-lg">
-          Recent Activity in {groupName} for {firstName}
+          Recent Activity in {groupName} for {user.firstName}
         </h2>
       </div>
       <div className="px-4 pb-2 m-auto">
-        <UserActivityOfGroupCell userId={userId} groupId={groupId} />
+        <UserActivityOfGroupCell userId={user.id} groupId={groupId} />
       </div>
     </div>
   )
--- web/src/components/StudentPointsCard/StudentPointsCard.js
+++ web/src/components/StudentPointsCard/StudentPointsCard.js
@@ -1,16 +1,14 @@
-import StudentGroupPointValueCell from 'src/components/cells/StudentGroupPointValueCell/StudentGroupPointValueCell'
-
-const StudentPointsCard = ({ firstName, lastName, userId, groupId, totalPoints, userGroupPoints }) => {
+const StudentPointsCard = ({ user }) => {
+  const { firstName, lastName, points, groupPoints = [] } = user
   return (
     <div className="h-36 flex-grow-0 flex-shrink-0 white-box mt-1 mb-4 flex flex-col relative">
       <h1 className="text-3xl font-display mb-4">
         {firstName} {lastName}
       </h1>
       <div className="flex absolute bottom-2 right-4 items-end">
-        <div className='mb-1'>
+        <div className="mb-1">
           <span className="font-display text-6xl text-green mr-2">
-            {/* <StudentGroupPointValueCell userId={userId} groupId={groupId} /> */}
-            {userGroupPoints}
+            {groupPoints[0].points}
           </span>
           <span className="font-display text-3xl text-green mr-6">
             group kudos
@@ -19,7 +17,7 @@ const StudentPointsCard = ({ firstName, lastName, userId, groupId, totalPoints, 
       </div>
       <div className="flex absolute bottom-2 left-4 items-end">
         <span className="font-display text-5xl text-gray-400 mr-2">
-          {totalPoints}
+          {points}
         </span>
         <span className="font-display text-xl text-gray-400 pb-1">
           total kudos
