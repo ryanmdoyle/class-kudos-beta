dd3a8db8839cb672a997dd86bd34515084a8e8fa
Ryan Doyle <doylecodes@gmail.com>
2021-09-11 16:52:50 -0700

    prettier & remove comment code

--- web/src/components/cells/StudentGroupPageCell/StudentGroupPageCell.js
+++ web/src/components/cells/StudentGroupPageCell/StudentGroupPageCell.js
@@ -30,7 +30,13 @@ export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ userId, groupId, group, user, groupPointsOfUser }) => {
+export const Success = ({
+  userId,
+  groupId,
+  group,
+  user,
+  groupPointsOfUser,
+}) => {
   return (
     <>
       <MetaTags
@@ -38,21 +44,28 @@ export const Success = ({ userId, groupId, group, user, groupPointsOfUser }) => 
         description={`Student view for ${group?.name}`}
       />
       <div className="w-full h-full p-4 relative grid grid-cols-2 grid-rows-6 gap-2">
-        <div className='col-span-1 row-span-6 flex flex-col'>
+        <div className="col-span-1 row-span-6 flex flex-col">
           {/* Points */}
-          <div className='flex w-full justify-between'>
+          <div className="flex w-full justify-between">
             <div className="h-32 w-1/2 white-box relative p-2 mb-4 flex items-center mr-2">
               {/* GROUP Points */}
-              <div className='flex flex-col items-center w-full'>
-                <span className="text-5xl text-green">{groupPointsOfUser?.points}</span>
+              <div className="flex flex-col items-center w-full">
+                <span className="text-5xl text-green">
+                  {groupPointsOfUser?.points}
+                </span>
                 <span className="text-lg text-green">
-                  {group?.name} {`${groupPointsOfUser?.points === 1 ? 'kudo' : 'kudos' || 'kudos'}`}
+                  {group?.name}{' '}
+                  {`${
+                    groupPointsOfUser?.points === 1
+                      ? 'kudo'
+                      : 'kudos' || 'kudos'
+                  }`}
                 </span>
               </div>
             </div>
             <div className="h-32 w-1/2 white-box relative p-2 mb-4 flex items-center ml-2">
               {/* TOTAL */}
-              <div className='flex flex-col items-center w-full'>
+              <div className="flex flex-col items-center w-full">
                 <span className="text-5xl text-gray-400">{user?.points}</span>
                 <span className="text-lg text-gray-500">
                   total {`${user?.points === 1 ? 'kudo' : 'kudos' || 'kudos'}`}
@@ -60,27 +73,31 @@ export const Success = ({ userId, groupId, group, user, groupPointsOfUser }) => 
               </div>
             </div>
           </div>
-          <div className="h-full white-box relative p-2 flex items-center flex flex-col">
-            {/* Rewards */}
-            <h2 className="font-display text-2xl mb-2 p-2">Rewards Available</h2>
-            <RewardsOfGroupStudentCell
-              groupId={groupId}
-              userId={userId}
-              totalPoints={user?.points}
-              groupPoints={groupPointsOfUser?.points}
-            />
+          <div className="bg-white rounded-md shadow h-full overflow-y-auto">
+            {/* use custom padding over 'white-box for list to scroll behind header */}
+            <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4 z-10">
+              <h2 className="font-display text-2xl z-10">Rewards Available</h2>
+            </div>
+            <div className="px-4 z-0">
+              <RewardsOfGroupStudentCell
+                groupId={groupId}
+                userId={userId}
+                totalPoints={user?.points}
+                groupPoints={groupPointsOfUser?.points}
+              />
+            </div>
           </div>
         </div>
         {/* Activity */}
         <div className="relative col-span-1 row-span-6">
-          <div className="bg-white rounded-md shadow h-full overflow-y-scroll">
+          <div className="bg-white rounded-md shadow h-full overflow-y-auto">
             {/* use custom padding over 'white-box for list to scroll behind header */}
-            <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4">
+            <div className="w-full bg-white sticky top-0 mb-2 pl-4 pt-4 z-10">
               <h2 className="font-display text-2xl">
                 Recent Feedback {group?.name ? `in ${group.name}` : null}
               </h2>
             </div>
-            <div className="px-4">
+            <div className="px-4 z-0">
               <UserActivityOfGroupCell groupId={groupId} userId={userId} />
             </div>
           </div>
@@ -89,33 +106,3 @@ export const Success = ({ userId, groupId, group, user, groupPointsOfUser }) => 
     </>
   )
 }
-
-
-// {/* Points */}
-// <div className="h-32 white-box relative p-2 mb-4 flex items-center">
-// <div className="w-full px-4 flex justify-between xl:justify-around">
-//   {/* GROUP Points */}
-//   <div>
-//     <span className="text-7xl text-green">{groupPointsOfUser.points}</span>
-//     <span className="text-lg ml-2 text-green self-end">
-//       {group.name} {`${groupPointsOfUser.points === 1 ? 'kudo' : 'kudos' || 'kudos'}`}
-//     </span>
-//   </div>
-//   {/* TOTAL */}
-//   <div className='flex items-end'>
-//     <span className="text-6xl text-gray-400">{user.points}</span>
-//     <span className="ml-2 text-gray-500 self-end">
-//       total {`${user.points === 1 ? 'kudo' : 'kudos' || 'kudos'}`}
-//     </span>
-//   </div>
-// </div>
-// </div>
-// {/* Rewards */}
-// <div className="white-box p-2 h-full">
-// <h2 className="font-display text-2xl mb-2 p-2">Rewards Available</h2>
-// <RewardsOfGroupStudentCell
-//   groupId={groupId}
-//   userId={userId}
-//   availablePoints={user.points}
-// />
-// </div>
